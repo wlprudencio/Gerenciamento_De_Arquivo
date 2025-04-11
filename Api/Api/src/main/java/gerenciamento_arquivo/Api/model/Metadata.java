aa835651b68523a5cfb3737b78cd254cda2b0408
Samuel Santos Miranda <138618261+SamuelSantos20@users.noreply.github.com>
2025-04-11 16:37:27 -0300

    Teste de branch e envio das Classes
    Tô testando a branch e enviando as classes que eu fiz.

--- Api/Api/src/main/java/gerenciamento_arquivo/Api/model/Metadata.java
+++ Api/Api/src/main/java/gerenciamento_arquivo/Api/model/Metadata.java
@@ -2,10 +2,8 @@ package gerenciamento_arquivo.Api.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.proxy.HibernateProxy;
 
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 @Table(name = "metadata")
@@ -14,6 +12,7 @@ import java.util.Objects;
 @Getter
 @Setter
 @ToString
+@EqualsAndHashCode
 public class Metadata {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,19 +26,6 @@ public class Metadata {
     private String description;
 
 
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        Metadata metadata = (Metadata) o;
-        return getMetadataid() != null && Objects.equals(getMetadataid(), metadata.getMetadataid());
-    }
-
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
-    }
 }
+
+
